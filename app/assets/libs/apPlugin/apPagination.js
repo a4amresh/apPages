23f284d3d70e7299ba84ff9aee3a546d480fca4d
Amresh Kumar <amresh.kumar@ids-logic.local>
2016-07-19 11:58:16 +0530

    Add project files

--- app/assets/libs/apPlugin/apPagination.js
+++ app/assets/libs/apPlugin/apPagination.js
@@ -163,27 +163,3 @@
         mixiTupState: false
     };
 }(jQuery, window, document));
-$(document).ready(function() {
-    $("#blog").apPagination({
-        selctor: {
-            target: 'aside',
-            targetWrap: '',
-            paginationWrap: '.ap-pagination'
-        },
-        animation: {
-            enabled: true,
-            animationIn: 'zoomIn'
-        },
-        button: {
-            btnWrap: 'li',
-            btnClass: 'page-btn',
-            prevClass: 'prev', // Class name without . (dot)
-            nextClass: 'next', // Class name without . (dot)
-            activeClass: 'active', // Class name without . (dot)
-            nextText: 'Next > ',
-            prevText: ' < Prev'
-        },
-        perPage: '3',
-        mixiTupState: false
-    });
-});
